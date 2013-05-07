f41a3ed194fb0e217671a33b5cb82c32c4193ec4
tiposaurio <tiposaurio@gmail.com>
2013-05-07 18:06:45 -0500

    tiposaurio

--- mensaje.sh
+++ mensaje.sh
@@ -8,6 +8,7 @@ git remote add origin git@github.com:tiposaurio/mejorandola.git
 git add .
 git status
 git commit -a  -m  "este es mi archivo de git"
+git clone git@github.com:tiposaurio/mejorandola.git
 git status
 git pull origin master
 git push origin master
